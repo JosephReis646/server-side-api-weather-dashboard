9d4b0667d506d235d12e6da43a2945c5b1451895
joseph reis <josephreis646@gmail.com>
2023-02-21 20:52:03 -0500

    flexbox in CSS and API key, Dom dependencies

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
-// DEPENDENCIES (DOM Elements)
-// DATA / STATE / GLOBAL VARIABLES
-// FUNCTIONS
-// USER INTERACTIONS
-// INITIALIZATION
+// API key (insert your own API key here)
+const API_KEY = "096e002965be779548e85ad4cca06d1c";
+
+// Get DOM elements
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const searchHistory = document.getElementById("search-history");
+const searchHistoryList = searchHistory.querySelector("ul");
+const currentWeatherContainer = document.getElementById("current-weather-container");
+const forecastContainer = document.getElementById("forecast-container");
 
