dc456328f53e2bed9b47671f37403c444442e0f6
joseph reis <josephreis646@gmail.com>
2023-02-21 21:21:56 -0500

    Function to search for city weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,36 @@ searchHistoryList.addEventListener("click", function(event) {
   const searchTerm = event.target.textContent;
   searchCity(searchTerm);
 });
+
+// Function to search for city weather data
+function searchCity(searchTerm) {
+    // Add search term to search history
+    addSearchTerm(searchTerm);
+  
+    // Clear previous weather data
+    clearWeatherData();
+  
+    // API call to retrieve current weather data for the city
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        displayCurrentWeather(data);
+      })
+      .catch(error => {
+        console.error(error);
+        alert("Error retrieving weather data. Please try again.");
+      });
+  
+    // API call to retrieve 5-day forecast data for the city
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        displayForecast(data);
+      })
+      .catch(error => {
+        console.error(error);
+        alert("Error retrieving weather data. Please try again.");
+      });
+  }
+  
+  
